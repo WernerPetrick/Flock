2f27ae625d0ea8014b478de19d7e42e539768566
WernerPetrick <128157845+WernerPetrick@users.noreply.github.com>
2024-09-09 04:30:08 +0200

    Initial Commit

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,41 @@
+class BirdsController < ApplicationController
+
+  def index
+    @birds = Bird.all
+  end
+
+  def show 
+    @bird = Bird.find(params[:id])
+  end
+
+  def new
+    @bird = Bird.new
+  end
+
+  def create
+    @bird = Bird.new(bird_params)
+
+    if @bird.save
+      redirect_to @bird
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @bird = Bird.find(params[:id])
+  end
+
+  def update
+    @bird = @bird = Bird.find(params[:id])
+
+    if @bird.update(article_params)
+      redirect_to @bird
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :birds
+  resources :users
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  get 'about', to: 'pages#about'
+  get 'join', to: 'pages#join'
+  get 'faq', to: 'pages#faq'
+  get 'contact', to: 'pages#contact'
+
+  # Defines the root path route ("/")
+  root "pages#landing"
+end
